c2132c04bde993179f205a160d040a72e6105d2f
Antonio Rios <Antoniorioscarballo17@gmail.com>
2023-09-08 23:34:46 -0400

    updated script - 08-09-2023

--- date.py
+++ date.py
@@ -23,14 +23,32 @@ date_name = input("What is your date's name")
 # set budget
 
 # show the menu
-  # show selection of random 5 drinks
+  
   # randomize prices of all the drinks
   # reduce budget by drink price
-
+# show selection of random 5 drinks
 def get_menu():
     url = "https://the-cocktail-db3.p.rapidapi.com/"
     response = requests.get(url, headers=keys.headers)
     data = response.json()
+    counter = 0
+    drinks = {}
+    while counter < 5:
+        counter +=1
+        drinks[str(counter)]=(data[r(1,132)]["title"])
+    def show_menu():
+        for key, values in drinks.items():
+            print(key,values)
+    return show_menu()
+get_menu()
+
+# randomize prices of all the drinks
+
+
+
+# reduce budget by drink price
+
+
 # function to track wellness of date
 
-# 
+
--- test-api.py
+++ test-api.py
@@ -1,33 +1,20 @@
 import requests
 import keys
 from random import randint as r
-url = "https://the-cocktail-db3.p.rapidapi.com/"
 
-
-response = requests.get(url, headers=keys.headers)
-
-data = response.json()
-
-# for item in data:
-    # print(item["title"])
-
-# get the ids 
-# for id in data:
-    # print(id["id"])
-
-# print(len(data))
-
-# print(r(0,9))
 def get_menu():
+    url = "https://the-cocktail-db3.p.rapidapi.com/"
+    response = requests.get(url, headers=keys.headers)
+    data = response.json()
     counter = 0
     drinks = {}
     while counter < 5:
         counter +=1
         drinks[str(counter)]=(data[r(1,132)]["title"])
-    def print_menu():
+    def show_menu():
         for key, values in drinks.items():
-            print("1", key,values)
-    return print_menu()
-print(get_menu())
+            print(key,values)
+    return show_menu()
+get_menu()
 
 # print(data)
