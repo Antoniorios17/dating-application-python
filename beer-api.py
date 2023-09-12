c7b5d473d0dcf1ecb13b2dbfa14d0e57bd1c2fdc
Antonio Rios <Antoniorioscarballo17@gmail.com>
2023-09-12 13:50:55 -0400

    updated script - 12-09-2023

--- beer-api.py
+++ beer-api.py
@@ -1,4 +1,36 @@
 import requests
 import json
+import random
+from random import randint
 
-url = https://api.punkapi.com/v2/
+url = "https://api.punkapi.com/v2/beers"
+r = requests.get(url)
+data = json.loads(r.text)
+# print(json.dumps(data[0], indent = 4))
+# print(data)
+# for drink in data:
+#     print(drink['name'])
+# print(data[0]['food_pairing'][randint(0, len([data[0]['food_pairing']])+1)])
+
+
+def get_menu():
+    url = "https://api.punkapi.com/v2/beers"
+    r = requests.get(url)
+    data = json.loads(r.text)
+    counter = 0
+    drinks = {}
+    print("Please select your drink of choice\n")
+    while counter < 5:
+        counter +=1
+        food_selection = random.choice(data[randint(0,len(data))]["food_pairing"])
+        drinks[str(counter)]= food_selection
+        
+    def show_menu():
+        for key, values in drinks.items():
+            print(key,values)
+    return show_menu()
+
+get_menu()
+
+
+
--- date.py
+++ date.py
@@ -19,25 +19,24 @@ budget = input("Please insert your budget: ")
   # randomize prices of all the drinks
   # reduce budget by drink price
 # show selection of random 5 drinks
-def get_api():
-    url = "https://the-cocktail-db3.p.rapidapi.com/"
-    response = requests.get(url, headers=keys.headers)
-    data = response.json()
-    return data
+
 def get_menu():
-    url = "https://the-cocktail-db3.p.rapidapi.com/"
-    response = requests.get(url, headers=keys.headers)
-    data = response.json()
+    url = "https://api.punkapi.com/v2/beers"
+    r = requests.get(url)
+    data = json.loads(r.text)
     counter = 0
     drinks = {}
     print("Please select your drink of choice\n")
     while counter < 5:
         counter +=1
-        drinks[str(counter)]=(data[r(1,132)]["title"])
+        food_selection = random.choice(data[randint(0,len(data))]["food_pairing"])
+        drinks[str(counter)]= food_selection
+        
     def show_menu():
         for key, values in drinks.items():
             print(key,values)
     return show_menu()
+
 get_menu()
 
 # randomize prices of all the drinks
