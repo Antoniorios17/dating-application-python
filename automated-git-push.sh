18368314dcdfa95013fdc5c0aa432f2f8274d5a2
Antonio Rios <Antoniorioscarballo17@gmail.com>
2023-09-12 18:08:17 -0400

    updated script - 09-12-2023

--- automated-git-push.sh
+++ automated-git-push.sh
@@ -3,5 +3,5 @@
 # The script will automatically push to the repository when executed
 # it will include the date as part of the commit message
 git add .
-git commit -m "updated script - $(date +'%d-%m-%Y')"
+git commit -m "updated script - $(date +'%m-%d-%Y')"
 git push origin main
