4933364d7ecf065d398d97c7c885479fb14631b8
Antonio Rios <Antoniorioscarballo17@gmail.com>
2023-09-12 13:58:23 -0400

    updated script - 12-09-2023

--- date.py
+++ date.py
@@ -4,12 +4,14 @@ import keys #importing the api keys from another local file
 from random import randint as r
 import api
 
-# welcome statement
-print("Welcome to the date simulator\n")
-# name of customer
-# name = input("what is your name: ")
-# name of date
-# date_name = input("What is your date's name: ")
+# # welcome statement
+# print("Welcome to the date simulator\n")
+# # name of customer
+# name = input("What is your name: ").capitalize()
+# # name of date
+# date_name = input("What is your date's name: ").capitalize()
+# print("")
+# print(f"****** Welcome {name} and {date_name}! *******\n")
 
 
 # set budget
@@ -20,27 +22,27 @@ budget = input("Please insert your budget: ")
   # reduce budget by drink price
 # show selection of random 5 drinks
 
-def get_menu():
-    url = "https://api.punkapi.com/v2/beers"
-    r = requests.get(url)
-    data = json.loads(r.text)
-    counter = 0
-    drinks = {}
-    print("Please select your drink of choice\n")
-    while counter < 5:
-        counter +=1
-        food_selection = random.choice(data[randint(0,len(data))]["food_pairing"])
-        drinks[str(counter)]= food_selection
+# def get_menu():
+#     url = "https://api.punkapi.com/v2/beers"
+#     r = requests.get(url)
+#     data = json.loads(r.text)
+#     counter = 0
+#     drinks = {}
+#     print("Please select your drink of choice\n")
+#     while counter < 5:
+#         counter +=1
+#         food_selection = random.choice(data[randint(0,len(data))]["food_pairing"])
+#         drinks[str(counter)]= food_selection
         
-    def show_menu():
-        for key, values in drinks.items():
-            print(key,values)
-    return show_menu()
+#     def show_menu():
+#         for key, values in drinks.items():
+#             print(key,values)
+#     return show_menu()
 
-get_menu()
+# get_menu()
 
 # randomize prices of all the drinks
-def price_per_drink(price):
+def reduce_budget(price):
     price = r(10,15)
     return price
 
