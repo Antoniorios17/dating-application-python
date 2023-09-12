ec5cbd7cb5320f308981a148c2d17d8cc29261ed
Antonio Rios <Antoniorioscarballo17@gmail.com>
2023-09-12 17:40:16 -0400

    updated script - 12-09-2023

--- date.py
+++ date.py
@@ -4,18 +4,18 @@ from random import randint
 import random
 
 
-# # welcome statement
-# print("Welcome to the date simulator\n")
-# # name of customer
-# name = input("What is your name: ").capitalize()
-# # name of date
-# date_name = input("What is your date's name: ").capitalize()
-# print("")
-# print(f"****** Welcome {name} and {date_name}! *******\n")
+# welcome statement
+print("Welcome to the date simulator\n")
+# name of customer
+name = input("What is your name: ").capitalize()
+# name of date
+date_name = input("What is your date's name: ").capitalize()
+print("")
+print(f"****** Welcome {name} and {date_name}! *******\n")
 
 
 # set budget
-budget = int(input("Please insert your budget: "))
+budget = int(input("Please insert your budget: \n"))
 
 # show the menu
 
@@ -40,8 +40,9 @@ get_menu()
 
 # randomize prices of all the menu
 def reduce_budget():
-    selection = input("Please select an item from the menu\n")
-    price = randint(10,15)
+    print("")
+    selection = input("Please select an item from the menu: ")
+    price = randint(15,30)
     print(f'You selected {menu[selection]} and the price is ${price}')
     global budget
     budget = budget - price
@@ -53,4 +54,11 @@ reduce_budget()
 # function to track wellness of date
 
 while True:
-    
+    choice = input("Would you like to order more? (yes/no) ").strip().lower()
+    if choice[0] == "y":
+        get_menu()
+        reduce_budget()
+    elif choice[0] == 'n':
+        print(f"Thank you for coming {name} and {date_name}")
+        break
+
